0f18d6fb69214e17afb1c296017d8d5bc21b6d38
eddaww <cai01edwin@gmail.com>
2024-10-02 23:26:33 -0700

    images updated

--- codes/conf/database.js
+++ codes/conf/database.js
@@ -21,7 +21,7 @@ const pool = mysql.createPool({
   host:'localhost',
   user:'root',
   database: 'artisanaura',
-  password: '9210',
+  password: '',
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 100,
